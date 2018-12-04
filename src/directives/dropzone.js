cfb727e535c29675ffa40b500035ffaa00c2ee2e
Jon <cookie.jon@gmail.com>
2018-12-04 16:12:28 +1100

    ox to en

--- src/directives/dropzone.js
+++ src/directives/dropzone.js
@@ -1,37 +1,72 @@
 //  Jon's dropzone directive!
 //
 
-let fileinput = document.createElement('input').setAttribute('type', 'file')
+/* eslint-disable */
+// let fileinput = document.createElement('input').setAttribute('type', 'file')
 
 export default {
+    bind: function (el) {
+        const handler = () => {  }
+        el.addEventListener('someEvent', handler)
+        el.$destroy = () => el.removeEventListener('someEvent', handler)
+        },
+        unbind: function (el) {
+        el.$destroy()
+    }
 
-    mounted() {
+}
 
-        @dragenter.stop.prevent="doDragEnter"
-        @dragover.stop.prevent="doDragOver"
-        @drop.stop.prevent="doDrop"
+/*
+  mounted() {
 
-        this.$el.addEventListener('dragenter', (e) => {e.stopPropagation(); e.preventDefault(); this.$emit('enter', e)}
-        this.$el.addEventListener('dragster:leave', (e) => this.$emit('leave', e))
+        // this.$el.addEventListener('dragenter', (e) => {this.$emit('dragenter', e)
+        // this.$el.addEventListener('dragover', (e) => {this.$emit('enter', e)
+        this.$el.addEventListener('drop', (e) => {this.$emit('enter', e)
     },
 
     bind: function (el, binding, vnode) {
         var s = JSON.stringify
         alert('hi')
         let out = 
-        'name: '       + s(binding.name) + '<br>' +
-        'value: '      + s(binding.value) + '<br>' +
-        'expression: ' + s(binding.expression) + '<br>' +
-        'argument: '   + s(binding.arg) + '<br>' +
-        'modifiers: '  + s(binding.modifiers) + '<br>' +
-        'vnode keys: ' + Object.keys(vnode).join(', ')
+            'name: '       + s(binding.name) + '<br>' +
+            'value: '      + s(binding.value) + '<br>' +
+            'expression: ' + s(binding.expression) + '<br>' +
+            'argument: '   + s(binding.arg) + '<br>' +
+            'modifiers: '  + s(binding.modifiers) + '<br>' +
+            'vnode keys: ' + Object.keys(vnode).join(', ')
 
         console.log(out)
-    }
-    
-}
+    },
 
-/*
+  bind: function (el) {
+    const handler = () => { ... }
+    el.addEventListener('someEvent', handler)
+    el.$destroy = () => el.removeEventListener('someEvent', handler)
+  },
+  unbind: function (el) {
+    el.$destroy()
+  }
+    <div
+      ref="container"
+      :class='computedClass'
+      @xclick.stop.prevent="openFileInput"
+      @dragenter.stop.prevent="doDragEnter"
+      @dragover.stop.prevent="doDragOver"
+      @drop.stop.prevent="doDrop"
+    >
+    <slot></slot>    
+  </div>
+  <input
+    type="file"
+    ref="fileinput"
+    class="file-upload"
+    name="userprofile_picture"
+    multiple="multiple"
+    @change.stop.prevent="doSelect"
+  />
+ </div>
+
+
 <template>
 <div class="dropzone"><slot /></div>  
 </template>
